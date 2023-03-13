db33fe025a29d2d7d035e945bb1eb84581d7864a
killa-kyle <kylerose@gmail.com>
2023-03-13 17:40:01 -0400

    begin discord bot init

--- src/discord.ts
+++ src/discord.ts
@@ -1,10 +1,39 @@
-export default class DisordBot {
-    constructor() {
+import {
+    Client,
+    GatewayIntentBits,
+    Partials,
+    Events,
+} from 'discord.js'
 
+export default class DisordBot {
+    client: Client;
+    constructor() {
+        this.client = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.GuildIntegrations,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.DirectMessages,
+            ],
+            partials: [Partials.Channel]
+        });
+        // handle message events
+        this.client.on('messageCreate', (message) => this.handleMessage(message));
+        this.client.once(Events.ClientReady, c => {
+            console.log(`Ready! Logged in as ${c.user.tag}`);
+        });
     }
     init() {
 
         // init bot
         console.log('init discord bot')
+        this.client.login(process.env.DISCORD_BOT_TOKEN);
+    }
+    async handleMessage(message) {
+       
+        if (message.content === 'ping') {
+            await message.reply('Pong!');
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import discordBot from './discord'
 
 // load an instance of our Bot class
