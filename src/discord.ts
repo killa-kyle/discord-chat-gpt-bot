c1b84b9ca3ce814273dfb0d7d9fff5350b1d2f71
Kyle Rose <kylerose@gmail.com>
2023-03-15 12:39:42 -0400

    simple storage of user conversation

--- src/discord.ts
+++ src/discord.ts
@@ -6,7 +6,9 @@ import {
     DMChannel
 } from 'discord.js'
 import { getCompletion } from './openAI';
-
+import {readDb, writeDb, getSimilarTextFromDb, getCurrentDateTime, encodeToSHA256, cosineSimilarity, createDb, clearJsonFile} from './db';
+import crypto from 'crypto';
+const DB_FOLDER = "./conversations";
 export default class DisordBot {
     client: Client;
     constructor() {
@@ -46,13 +48,57 @@ export default class DisordBot {
 
         // remove the @mention from the message content
         if (!isDM) {
-            message.content = message.content.replace(`<@!${this.client.user?.id}>`, '').trim();
+            message.content = message.content.replace(`<@${this.client.user?.id}>`, '').trim();
         }
+
+        // cache the user's id so we can keep track of the bot's conversation with them
+        const userId = message.author.id as string;
+        
+        // create a new conversation for the user if one doesn't exist
+        const USER_DB_PATH = `${DB_FOLDER}/${userId}`;
+        await createDb(USER_DB_PATH);
+        // read the conversation from the db
+        const conversation = readDb(USER_DB_PATH);
+        
+        // get the parent message id if one exists from the conversation
+        const parentMessageId = conversation[conversation?.length - 1]?.id || crypto.randomUUID();
+     
+        // add the user's message to the conversation
+        const userMessage = {
+            id: crypto.randomUUID(),
+            parentMessageId,
+            user: userId,
+            role: "user",
+            content: message.content,
+            embedding: [],
+            timestamp: getCurrentDateTime()
+        }
+        conversation.push(userMessage)
+
+     
+        // write the conversation to the db
+        writeDb(conversation, USER_DB_PATH);
+       
 
         // get the bot's response
         const response = await getCompletion(message.content)
+        // add the bot's response to the conversation
+        const botMessage = {
+            id: crypto.randomUUID(),
+            parentMessageId: userMessage.id,
+            user: this.client.user?.id,
+            role: "assistant",
+            content: response,
+            embedding: [],
+            timestamp: getCurrentDateTime()
+        }
+        conversation.push(botMessage)
 
+        console.log({conversation})
+        // write the conversation to the db
+        writeDb(conversation, USER_DB_PATH);
+  
         await message.reply(response);
-     
+
     }
 }
